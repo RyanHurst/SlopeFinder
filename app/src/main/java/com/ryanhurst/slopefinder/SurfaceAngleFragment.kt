bd8ba66e1c740b85bb59dcbe078e7c53f73d8c4c
Ryan Hurst <ryan.hurst2@wework.com>
2020-10-12 20:41:52 -0600

    1.3

--- app/src/main/java/com/ryanhurst/slopefinder/SurfaceAngleFragment.kt
+++ app/src/main/java/com/ryanhurst/slopefinder/SurfaceAngleFragment.kt
@@ -13,7 +13,7 @@ import kotlinx.android.synthetic.main.fragment_current_angle.*
 class SurfaceAngleFragment : Fragment(R.layout.fragment_current_angle), SensorEventListener {
     override fun onSensorChanged(sensorEvent: SensorEvent) {
         val angle = SlopeService.getAngleFromSensorEvent(sensorEvent)
-        surface_angle_text.text = SlopeService.formatAngle(angle)
+        surface_angle_text?.text = SlopeService.formatAngle(angle)
     }
 
     override fun onAccuracyChanged(sensor: Sensor, i: Int) {}
